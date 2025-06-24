917271b35ff5bd9c4a91af5bd6157c1d21d43b57
vnkimchon <hungnguyenns94@gmail.com>
2025-06-24 05:16:36 -0700

    upload full ios app project

---
+++ ContentView.swift
@@ -0,0 +1,12 @@
+// SwiftUI ContentView
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Simple Photo Editor!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
